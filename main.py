f8c7b616497516fb5ca81d16666a8fac9aaaf5c6
cod3d <trotil0749@gmail.com>
2022-10-17 19:36:55 +0300

    asd

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def get_sessions(db_path: Path) -> Iterator[str]:
     :param db_path: путь к бд
     :return: Iterator[StringSession]
     """
-    print(f'поиск сессий в {db_path}...')
+    print(f'looking for sessions in {db_path}...')
 
     class NSessionTable(SessionTable):
         class Meta:
@@ -24,15 +24,15 @@ def get_sessions(db_path: Path) -> Iterator[str]:
             table_name = 'sessions'
 
     if not NSessionTable.table_exists():
-        print(f'База данных {db_path} не содержит таблицы сессий')
+        print(f'The database {db_path} does not contain `sessions` table')
         return ''
 
     record: NSessionTable
     for record in NSessionTable.select():
         if record.server_address is None or record.port is None or record.auth_key is None:
-            print(f'[{record.dc_id}] запись сессии невалидный. пропуск.')
+            print(f'[{record.dc_id}] record is invalid. skip.')
             continue
-        print(f'[{record.dc_id}] найдена сессия')
+        print(f'[{record.dc_id}] session found')
         r_ip = ipaddress.ip_address(record.server_address).packed
         yield CURRENT_VERSION + StringSession.encode(struct.pack(
             _STRUCT_PREFORMAT.format(len(r_ip)),
@@ -45,14 +45,14 @@ def get_sessions(db_path: Path) -> Iterator[str]:
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
-        print('Использование:\n\tpython main.py "D:\\telegram\\session.session"')
+        print('Usage:\n\tpython main.py "D:\\telegram\\session.session"')
     else:
         db_path_ = Path(sys.argv[1])
         if not db_path_.exists():
-            print(f'Путь к бд {db_path_} не существует')
+            print(f'The database path {db_path_} does not exist')
         else:
             sessions = list(get_sessions(db_path_))
-            print(f'Найдено {len(sessions)} сессий')
+            print(f'Found {len(sessions)} sessions')
 
             if len(sessions) > 0:
                 print('\n=== SESSIONS ===\n')
